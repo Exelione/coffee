8f42d551ef071f4934e35eb42acbf68cadb13ac6
Exelione <exelione@gmail.com>
2024-06-04 01:32:42 +0300

    historySection

---
+++ src/components/history/History.tsx
@@ -0,0 +1,28 @@
+
+import styles from './styles.module.css';
+
+export const History = () => {
+  return (
+    <section id={styles.history}>
+      <div className={styles.container}>
+        <div className={styles.historyWrap}>
+          <img src='/features2.png' alt="coffeeLogo" />
+          <img src='/Rectangle 14.png' alt="coffeeLogo" />
+          <img src='/features1.png' alt="coffeeLogo" />
+          <div className={styles.historyCard}>
+            <h2>Our history</h2>
+            <h3>Create a<br />
+              new story with us</h3>
+            <p>mauris rhoncus in imperdiet placerat. vestibu emismd nisl suscirpit ligula volutpat, a feyguat urn maxmaus. cars massa nibhtincidunt. donec et nibh maximus, est eu, mattis nuce. preasent ut quam quis quam venen
+              atis fringilla. morbi vastibulum id tells mmodo mattis. aliauam erat volutpal.</p>
+
+          </div>
+        </div>
+      </div>
+
+    </section>
+
+
+  )
+}
+
--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,26 +1,14 @@
 import { Banner } from '../banner/Banner.tsx';
-import styles from './styles.module.css';
+import { Features } from '../features/Features.tsx';
+import { History } from '../history/History.tsx';
+
 
 export const Main = () => {
   return (
     <>
     <Banner/>
-    <div className={styles.container}>
-      <ol className={styles.features}>
-        <li>
-          <span>01</span> Best Coffee Flavour
-          <img src='/features1.png' alt="featuresImg" />
-        </li>
-        <li>
-          <span>02</span> Place to get lost
-          <img src='/features2.png' alt="featuresImg" />
-        </li>
-        <li>
-          <span>03</span> Proper roesting
-          <img src='/features3.png' alt="featuresImg" />
-        </li>
-      </ol>
-    </div>
+    <Features/>
+    <History/>
     </>
   )
 }
