1c495789a3e06da561c0ddac25ac3219554cdb38
Exelione <exelione@gmail.com>
2024-06-07 00:20:00 +0300

    newSections(3)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 
 import './App.css'
 import { Banner } from './components/banner/Banner.tsx'
+import { CoffeeMachine } from './components/coffeeMachine/CoffeeMachine.tsx'
 import { Features } from './components/features/Features.tsx'
 import { Header } from './components/header/Header'
 import { History } from './components/history/History.tsx'
 import { MainProduct } from './components/mainProduct/MainProduct.tsx'
+import { Menu } from './components/menu/Menu.tsx'
+import { Testimonial } from './components/testimonial/Testimonial.tsx'
 
 function App() {
  
@@ -16,6 +19,9 @@ function App() {
     <Features/>
     <History/>
     <MainProduct/>
+    <CoffeeMachine/>
+    <Menu/>
+    <Testimonial/>
     </>
     
   )
